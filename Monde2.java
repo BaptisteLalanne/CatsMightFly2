91b0dfd0385768b1a8f061eb328157ea1d98c9b3
Fannycl <63225938+Fannycl@users.noreply.github.com>
2020-04-06 09:16:03 +0200

    Add files via upload

--- Monde2.java
+++ Monde2.java
@@ -3,12 +3,23 @@ import java.awt.*;
 
 public class Monde2 extends Monde {
     Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-    public Icon photo = new ImageIcon(new ImageIcon("Fondmonde2.png").getImage().getScaledInstance(3049,dim.height,Image.SCALE_DEFAULT));
-    public Icon photodiffilant= new ImageIcon(new ImageIcon("Fondmonde2deffilant.png").getImage().getScaledInstance(3049,dim.height,Image.SCALE_DEFAULT));
+    public Icon photo = new ImageIcon(new ImageIcon("Fondmonde2.png").getImage().getScaledInstance(3055,dim.height,Image.SCALE_DEFAULT));
+    public Icon photodiffilant= new ImageIcon(new ImageIcon("Fondmonde2deffilant.png").getImage().getScaledInstance(3055,dim.height,Image.SCALE_DEFAULT));
+    public Icon photochat = new ImageIcon("original2.gif");
+    
+    public int temp;
 
     public Monde2 (){
-
-
+        super();
+        vitesseDefilement=2;
+        gravite= 16;
+        obstacles = new Obstacle[3];
+        chat = new Chat(photochat,dim.height,350,gravite);
+        for (int i=0; i<obstacles.length; i++) {
+            obstacles[i]=new Epine(118,229,"feu",dim.width,dim.height-80, chat);
+            obstacles[i].placement(i);
+        }
+        temp=0;
 
     }
 
@@ -23,9 +34,29 @@ public class Monde2 extends Monde {
 
     public int [] tailleimage(){
         int[] tableau;
-        tableau = new int[]{dim.width,0 ,3049,dim.height};
+        tableau = new int[]{dim.width,0 ,3055,dim.height};
         return tableau;
     }
+    
+    public boolean collisionobstacles (){
+        super();
+        temp = temperature();
+        if (temp>1000){
+			perdu = true;
+		}
+        return perdu;
+    }
+    
+    public int temperature() {
+		if (chat.get_vitesse() == 0){
+			temp+=1;
+		} else if (temp>0) {
+			temp-=2; // Energie thermique = m*Cp*dT, flux thermique = E/temps ?
+			} else {
+				temp=0;
+			}
+		return temp;
+	}
 
     public double vitesseChute() {
         return 0;
